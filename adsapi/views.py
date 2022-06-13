a4884ca543ada8d6fa6cedb654fe7a7e25349cef
italiya07 <60642468+italiya07@users.noreply.github.com>
2022-06-13 17:15:04 +0530

    Update views.py

--- adsapi/views.py
+++ adsapi/views.py
@@ -6,28 +6,28 @@ from elasticsearch import helpers
 from rest_framework.response import Response
 # Create your views here.
 
-# es=Elasticsearch(
-#     ['https://search-testfbadslib-arkod77br4pscry5r4hqaacnfy.us-east-1.es.amazonaws.com/'],
-#     http_auth=('jeylearner2022', 'Jey@aws1290'),
-#     )
+es=Elasticsearch(
+    ['https://search-testfbadslib-arkod77br4pscry5r4hqaacnfy.us-east-1.es.amazonaws.com/'],
+    http_auth=('jeylearner2022', 'Jey@aws1290'),
+    )
 
-# es.indices.create(index='scraping_project',ignore=400)
+es.indices.create(index='scraping_project',ignore=400)
 
 class getAllAds(viewsets.ViewSet):
     def list(self,request):
-#         print(es.ping())
-#         query={
-#             "size": 10000,
-#             "query": {
-#                 "match_all": {}
-#             }
-#         }
+        print(es.ping())
+        query={
+            "size": 10000,
+            "query": {
+                "match_all": {}
+            }
+        }
 
-#         res=es.search(index="scraping_project",body=query)
-#         data=[]
-#         if res["hits"]["hits"]:
-#             for d in res["hits"]["hits"]:
-#                 data.append(d["_source"])
-#             return Response({"data":data,"msg":"Data fetched successfully"}) 
+        res=es.search(index="scraping_project",body=query)
+        data=[]
+        if res["hits"]["hits"]:
+            for d in res["hits"]["hits"]:
+                data.append(d["_source"])
+            return Response({"data":data,"msg":"Data fetched successfully"}) 
         return Response("Data is not available")
 
