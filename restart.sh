e22d9682b5b5107947d121fdaa83fec7856dbaf4
godhanidenis <godhanidenis@gmail.com>
2022-06-14 18:15:31 +0530

    cross-header added

--- restart.sh
+++ restart.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+cd /home/ec2-user/fbadsfinal
+source venv/bin/activate
+pip install -r requirements.txt
+
+python manage.py makemigrations
+python manage.py migrate --noinput
+python manage.py collectstatic
+
+
 sudo systemctl restart gunicorn
 sudo systemctl daemon-reload
 sudo systemctl restart gunicorn.socket gunicorn.service
