8836416105d37571afe7a9d0728396c157214d66
godhanidenis <godhanidenis@gmail.com>
2022-07-13 11:26:18 +0530

    elasticsearch version changed

--- adsapi/views.py
+++ adsapi/views.py
@@ -715,15 +715,18 @@ def cancel_subscription(request):
 def fetch_payment_method(request):
     stripe.api_key = API_KEY
     sub_obj=Subscription_details.objects.filter(user=request.user).first()
-    payment_details=stripe.PaymentMethod.list(
-        customer=sub_obj.customer_id,
-        type="card",
-    )
-    sub_status=stripe.Subscription.retrieve(
-        sub_obj.subscription_id,
-    )
-    end_date=sub_status.current_period_end
-    r=rh.ResponseMsg(data={"status":sub_status.status,"end_date":datetime.datetime.utcfromtimestamp(end_date).strftime('%b %d, %Y'),"plan_type":sub_status.plan.nickname,"paydement_method_id":payment_details.data[0].id,"card_brand":payment_details.data[0].card["brand"],"country":payment_details.data[0].card["country"],"exp_month":payment_details.data[0].card["exp_month"],"exp_year":payment_details.data[0].card["exp_year"],"last4":payment_details.data[0].card["last4"],"funding":payment_details.data[0].card["funding"]},error=False,msg="Thank You for Payment !!!")
+    if sub_obj.subscription_id:
+        payment_details=stripe.PaymentMethod.list(
+            customer=sub_obj.customer_id,
+            type="card",
+        )
+        sub_status=stripe.Subscription.retrieve(
+            sub_obj.subscription_id,
+        )
+        end_date=sub_status.current_period_end
+        r=rh.ResponseMsg(data={"status":sub_status.status,"end_date":datetime.datetime.utcfromtimestamp(end_date).strftime('%b %d, %Y'),"plan_type":sub_status.plan.nickname,"paydement_method_id":payment_details.data[0].id,"card_brand":payment_details.data[0].card["brand"],"country":payment_details.data[0].card["country"],"exp_month":payment_details.data[0].card["exp_month"],"exp_year":payment_details.data[0].card["exp_year"],"last4":payment_details.data[0].card["last4"],"funding":payment_details.data[0].card["funding"]},error=False,msg="Thank You for Payment !!!")
+        return Response(r.response, status=status.HTTP_200_OK)
+    r=rh.ResponseMsg(data={},error=False,msg="Subscription is cancelled")
     return Response(r.response, status=status.HTTP_200_OK)
 
 # @api_view(['GET'])
