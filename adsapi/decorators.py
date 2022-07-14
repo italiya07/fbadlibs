5cf28c9d9d2d25f13f4381b60814711f49684b09
godhanidenis <godhanidenis@gmail.com>
2022-07-14 13:02:37 +0530

    subscripotion validation added

--- adsapi/decorators.py
+++ adsapi/decorators.py
@@ -4,6 +4,7 @@ import stripe
 from .models import *
 from rest_framework.response import Response
 from rest_framework import status
+import utils.response_handler as rh
 API_KEY = config("STRIPE_SECRET_KEY")
 
 def subscription_required(view_func):
@@ -12,13 +13,15 @@ def subscription_required(view_func):
         sub_obj=Subscription_details.objects.filter(user=request.user).first()
         if sub_obj:
             if sub_obj.sub_status == False:
-                return Response("Subscription Required", status=status.HTTP_200_OK)
+                r=rh.ResponseMsg(data={"subscription":False},error=False,msg="Subscription Required")
+                return Response(r.response,status=status.HTTP_200_OK)
 
             sub_status=stripe.Subscription.retrieve(
                 sub_obj.subscription_id,
             )
             if sub_status.status == "active":
                 return view_func(request,*args,**kwargs)
-        return Response("Subscription Required", status=status.HTTP_200_OK)
+        r=rh.ResponseMsg(data={"subscription":False},error=False,msg="Subscription Required")
+        return Response(r.response,status=status.HTTP_200_OK)
     return wrap
     
