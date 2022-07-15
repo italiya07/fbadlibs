6e8fe2433d2de2aed3f52ec644b279142f102a1f
denisgodhani <godhanidenis@gmail.com>
2022-07-15 16:18:30 +0530

    resolved accesstoken expired issues

--- adsapi/middleware.py
+++ adsapi/middleware.py
@@ -44,8 +44,10 @@ class SimpleMiddleware:
 
                 response = self.get_response(request)
 
+                print(settings)
+
                 response.set_cookie(
-                                key = settings.SIMPLE_JWT['AUTH_COOKIE'], 
+                                key = 'access_token', 
                                 value = access,
                                 expires = datetime.datetime.utcnow()+timedelta(seconds=int(config("ACCESS_TOKEN_EXPIRE_TIME_SECONDS"))),
                                 secure = settings.SIMPLE_JWT['AUTH_COOKIE_SECURE'],
