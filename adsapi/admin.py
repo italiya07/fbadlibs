3a5d01fa8e225682688e97d4e1f651e99599b5b4
godhanidenis <godhanidenis@gmail.com>
2022-08-03 11:22:11 +0530

    all data erased

--- adsapi/admin.py
+++ adsapi/admin.py
@@ -12,5 +12,15 @@ class Useradmin(admin.ModelAdmin):
     list_display = ['email']
 admin.site.register(User,Useradmin)
 
-admin.site.register(SaveAds)
+class SaveAdsadmin(admin.ModelAdmin):
+    search_fields=['user']
+    list_filter=['user']
+    list_display=['user','ad']
+admin.site.register(SaveAds,SaveAdsadmin)
+
+class Subadmin(admin.ModelAdmin):
+    search_fields=['user','sub_status']
+    list_filter=['user','sub_status','created_at']
+    list_display=['user','customer_id','subscription_id','sub_status','created_at']
+
 admin.site.register(Subscription_details)
